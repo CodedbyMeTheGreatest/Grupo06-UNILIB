279dc067c15eeb49ce56414ce7d048e78fda4b53
zeevvaa <se.pintoh@duocuc.cl>
2026-06-11 19:02:58 -0400

    prototipo_frontend
    Aquí se almacenan archivos correspondientes al desarrollo del diseño de la página web de UNILIB

---
+++ Frontend_UNILIB/interfaz_docente/offcanvas-navbar.js
@@ -0,0 +1,13 @@
+(() => {
+  'use strict'
+
+  document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
+
+  // Initialize tooltips for the sidebar
+  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(tooltipTriggerEl => {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
